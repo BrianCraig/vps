30bde7add1c7c75da8b342474d7c4253ae4919df
Brian Craig <briancraig@gmail.com>
2013-09-14 20:02:57 -0300

    Primera version 'Usable' ( Ponele :3 )

--- servidores/ejemplo2/main.js
+++ servidores/ejemplo2/main.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola mundo ! Servidor corriendo en el puerto '+process.env.PORT);
+  res.end('Segundo ejemplo de servidor, localizado en ./servidores/ejemplo2/ ! Servidor corriendo en el puerto '+process.env.PORT);
 }).listen(process.env.PORT || 80, '127.0.0.1'); // linea importantisima
 
 console.log('Servidor corriendo en el puerto '+process.env.PORT);
