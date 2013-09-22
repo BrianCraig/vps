f8c88f81e4d0081950e4c25b046e93939e005fe8
Brian Craig <briancraig@gmail.com>
2013-09-22 02:26:30 -0300

    Mejorando la App, se le agrego un archivo de configuracion manual, el boton para quitar las Apps, y la consola

---
+++ configuracion.js
@@ -0,0 +1,5 @@
+module.exports = {
+puertoEscucha : 80, //puerto donde se escucha todo
+dnsActivado : true, //Dns activado o no
+puertoInterfaz : 8080 //Puerto de la interfaz para manejarlo
+}
--- inicio.js
+++ inicio.js
@@ -1,8 +1,9 @@
+var configuracion = require('./configuracion.js'); // cargar objeto de la configuracion
 /*
 Cada uno de los archivos cargados acá, son independientes el uno con el otro.
 Se pueden modificar
 */
 
-var dns = require('./lib/dns.js'); // servicio DNS ( dnsd )
-var app = require('./lib/app.js'); // Nucleo de la App ( http-proxy + socket.io )
-var interfaz = require('./lib/interfaz.js'); // Servidor web en el puerto 8080 ( express.js )
+if(configuracion.dnsActivado) require('./lib/dns.js'); // servicio DNS opcional( dnsd )
+require('./lib/app.js'); // Nucleo de la App ( http-proxy + socket.io )
+require('./lib/interfaz.js'); // Servidor web en el puerto 8080 ( express.js )
