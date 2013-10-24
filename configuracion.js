72f7611eb08fb6ff83586d9978a256bc34047554
Brian Craig <briancraig@gmail.com>
2013-10-24 20:16:52 -0200

    Se reparo error en linux (no dejaba iniciar los procesos)

--- configuracion.js
+++ configuracion.js
@@ -1,5 +1,5 @@
 module.exports = {
 puertoEscucha : 80, //puerto donde se escucha todo
-dnsActivado : true, //Dns activado o no
+dnsActivado : false, //Dns activado o no
 puertoInterfaz : 8080 //Puerto de la interfaz para manejarlo
 }
